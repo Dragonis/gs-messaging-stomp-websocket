39ebba2c0e6c9fdd018de5e5b53f55a28fdcdfcf
wojciech.sasiela <wojciech.sasiela@zeto.lublin.pl>
2017-11-07 12:56:34 +0100

    legacy code

--- gs-messaging-stomp-websocket/src/main/java/hello/WebsocketController.java
+++ gs-messaging-stomp-websocket/src/main/java/hello/WebsocketController.java
@@ -1,19 +1,38 @@
 package hello;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RequestMapping("/")
+@RestController
 public class WebsocketController {
 
 	
-    //@GetMapping("app1") //get data from outside to localhost:8080/server/user
+    private String resourcePath = "./src/main/resources/ngapp/src/assets/i18n/";
+
+	//@GetMapping("websocket") //get data from outside to localhost:8080/server/user
+    @MessageMapping("")
     @SendTo("app1") // and prepared data send back to client
     public WebSocketMessage app1(WebSocketMessage message) throws Exception {
         //Thread.sleep(1000); // simulated delay
+    	try {
+            // Serialize Java object info JSON file.
+    		System.out.println("app1: test");
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.writeValue(new File(resourcePath+"app1.txt"), message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return message;
     }
     
@@ -24,4 +43,27 @@ public class WebsocketController {
     	return message;
     }
 
+    //http://localhost:4205/assets/i18n/pl.json
+    @GetMapping("/assets/i18n/pl.json")
+    public String pl_i18n(){
+    	String output = "";
+    	try {
+			output = new String(Files.readAllBytes(Paths.get(resourcePath + "pl.json")));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return output;    
+	}
+    
+    @GetMapping("/assets/i18n/en.json")
+    public String en_i18n(){
+    	String output = "";
+    	try {
+			output = new String(Files.readAllBytes(Paths.get(resourcePath  + "en.json")));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return output;    
+	}
+    
 }
